216f9b775b0e1da66415992ebce3a366b9b946db
joshfaberNCMC <90871011+joshfaberNCMC@users.noreply.github.com>
2023-02-05 16:53:30 -0500

    Delete CIS104 directory

--- CIS104/ex00/notHello.py
+++
@@ -1,1 +0,0 @@
-print('I came to class to chew bubblegum and kick ass... and I\'m all out of bubblegum')
--- CIS104/ex02/ex02-03.py
+++
@@ -1,4 +0,0 @@
-hrs = input("Enter Hours: ")
-rate = input("Enter Pay Rate: ")
-pay = float(hrs * rate)
-print("Pay:", pay)
